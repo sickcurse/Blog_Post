a9ea00ac4078f0fe24387690d8a96b8a592c45d9
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-07-22 18:13:46 -0400

    added const to username, title and content to create Elements to eventually create a form object. added the .trim to avoid interuptions of white spaces.

--- assets/javascript folder/form.js
+++ assets/javascript folder/form.js
@@ -3,4 +3,7 @@ const formEl = ddocument.getElementById('form');
 const handleFormSubmit = function (event) {
     event.preventDefault();
 
+  const usernameEl = document.getElementById('username').value.trim();
+  const titleEl = document.getElementById('title').value.trim();
+  const contentEl = document.getElementById('content').value.trim();
 }
