f89d17278afc0c8586d2214f301dada59928e503
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-07-22 18:31:07 -0400

    adding localStorage to the form.js file

--- assets/javascript folder/form.js
+++ assets/javascript folder/form.js
@@ -1,9 +1,28 @@
 const formEl = ddocument.getElementById('form');
 
-const handleFormSubmit = function (event) {
+const form = document.getElementById('form');
+const errorSpan = document.getElementById('error');
+
+form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-  const usernameEl = document.getElementById('username').value.trim();
-  const titleEl = document.getElementById('title').value.trim();
-  const contentEl = document.getElementById('content').value.trim();
-}
+    const usernameEl = document.getElementById('username').value.trim();
+    const titleEl = document.getElementById('title').value.trim();
+    const contentEl = document.getElementById('content').value.trim();
+
+    if (!username || !title || !content) {
+        errorSpan.textContent = 'Please fill out all fields.';
+        setTimeout(() => errorSpan.textContent = '', 3000);
+    } else {
+        errorSpan.textContent = ''; 
+    }
+
+    const formData = {
+        username: usernameEl,
+        title: titleEl,
+        content: contentEl,
+      };
+
+      storeLocalStorage(formData);
+      redirectPage();
+});
--- assets/javascript folder/script.js
+++ assets/javascript folder/script.js
@@ -7,20 +7,3 @@ if (backLink) {
 }
 
 
-const form = document.getElementById('form');
-const errorSpan = document.getElementById('error');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); 
-
-  const username = document.getElementById('username').value;
-  const title = document.getElementById('title').value;
-  const content = document.getElementById('content').value;
-
-  if (!username || !title || !content) {
-    errorSpan.textContent = "Please fill out all fields.";
-  } else {
-    errorSpan.textContent = ""; 
-    
-  }
-});
